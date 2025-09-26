db89566fbb05734b47dc55b6d3ec828744d53f41
cc <476840998@qq.com>
2025-09-26 14:48:05 +0800

    feat: 改造模版

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 
 import { GeistSans } from 'geist/font/sans';
 
-let title = 'Next.js + Postgres Auth Starter';
+let title = '学习卡片';
 let description =
   'This is a Next.js starter kit that uses NextAuth.js for simple email + password login and a Postgres database to persist the data.';
 
